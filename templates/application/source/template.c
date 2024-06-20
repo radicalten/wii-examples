2bbb15cd354f5d657f3e05a5adc48f06a6747dc1
radicalten <79894181+radicalten@users.noreply.github.com>
2024-06-19 20:10:17 -0500

    Update template.c

--- templates/application/source/template.c
+++ templates/application/source/template.c
@@ -9,19 +9,47 @@ static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
 //---------------------------------------------------------------------------------
-int main( int argc, char* argv[] ){
-//---------------------------------------------------------------------------------       
-	const int WIDTH = 640;
-        const int HEIGHT = 480;
-        SDL_Window* window = NULL;
-        SDL_Renderer* renderer = NULL;
-
-        SDL_Init(SDL_INIT_VIDEO);
-        window = SDL_CreateWindow("Hello SDL", WIDTH, HEIGHT, 0);
-        renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 0;
-    }
+int main(int argc, char **argv) {
+//---------------------------------------------------------------------------------
+	// Initialise the video system
+	VIDEO_Init();
+	// This function initialises the attached controllers
+	WPAD_Init();
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	//SYS_STDIO_Report(true);
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+	// Make the display visible
+	VIDEO_SetBlack(false);
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+	printf("Hello World!\n");
+	while(1) {
+		// Call WPAD_ScanPads each loop, this reads the latest controller states
+		WPAD_ScanPads();
+		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
+		// this is a "one shot" state which will not fire again until the button has been released
+		u32 pressed = WPAD_ButtonsDown(0);
+		// We return to the launcher application via exit
+		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
+		// Wait for the next frame
+		VIDEO_WaitVSync();
+	}
+	return 0;
+}
